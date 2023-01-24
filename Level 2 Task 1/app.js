827ce0e6ae73aa174b36c7ac14ecf1b57cf0cffc
ParthGharat27 <123398580+ParthGharat27@users.noreply.github.com>
2023-01-24 21:00:31 +0530

    Add files via upload

---
+++ Level 2 Task 1/app.js
@@ -0,0 +1,20 @@
+let value= document.getElementById('cal');
+function res()
+{
+    var res= document.getElementById("cal");
+    res.value=eval(res.value);
+
+}
+
+function number(value)
+{
+    var res= document.getElementById(cal);
+    cal.value += value;
+}
+
+function clearcal()
+{
+    document.getElementById("cal");
+    cal.value=" ";
+}
+
